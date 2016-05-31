0bccfdd51e62ce077903303e78c02a0cf326fe3f
Anders Haig <andershaig@gmail.com>
2016-05-30 18:32:10 -0700

    Center the tabs

--- app/js/app.js
+++ app/js/app.js
@@ -10,49 +10,49 @@ app.config(function ($mdThemingProvider, $mdIconProvider) {
     .icon("twitter"    , "./img/svg/twitter.svg"     , 512)
     .icon("phone"      , "./img/svg/phone.svg"       , 512);
 
-  var customPrimary = {
-        '50': '#f0f6f2',
-        '100': '#e0ede3',
-        '200': '#d0e4d5',
-        '300': '#bfdbc7',
-        '400': '#afd1b8',
-        '500': '#9FC8AA',
-        '600': '#8fbf9c',
-        '700': '#7fb58d',
-        '800': '#6eac7f',
-        '900': '#5ea371',
-        'A100': '#ffffff',
-        'A200': '#ffffff',
-        'A400': '#ffffff',
-        'A700': '#549365'
-    };
+  // var customPrimary = {
+  //       '50': '#f0f6f2',
+  //       '100': '#e0ede3',
+  //       '200': '#d0e4d5',
+  //       '300': '#bfdbc7',
+  //       '400': '#afd1b8',
+  //       '500': '#9FC8AA',
+  //       '600': '#8fbf9c',
+  //       '700': '#7fb58d',
+  //       '800': '#6eac7f',
+  //       '900': '#5ea371',
+  //       'A100': '#ffffff',
+  //       'A200': '#ffffff',
+  //       'A400': '#ffffff',
+  //       'A700': '#549365'
+  //   };
 
-    $mdThemingProvider
-        .definePalette('customPrimary', 
-                        customPrimary);
+  //   $mdThemingProvider
+  //       .definePalette('customPrimary', 
+  //                       customPrimary);
 
-    var customBackground = {
-        '50': '#838395',
-        '100': '#757589',
-        '200': '#69697b',
-        '300': '#5d5d6d',
-        '400': '#525260',
-        '500': '#464652',
-        '600': '#3a3a44',
-        '700': '#2f2f36',
-        '800': '#232329',
-        '900': '#17171b',
-        'A100': '#9090a1',
-        'A200': '#9e9eac',
-        'A400': '#acacb8',
-        'A700': '#0b0b0d'
-    };
+  //   var customBackground = {
+  //       '50': '#838395',
+  //       '100': '#757589',
+  //       '200': '#69697b',
+  //       '300': '#5d5d6d',
+  //       '400': '#525260',
+  //       '500': '#464652',
+  //       '600': '#3a3a44',
+  //       '700': '#2f2f36',
+  //       '800': '#232329',
+  //       '900': '#17171b',
+  //       'A100': '#9090a1',
+  //       'A200': '#9e9eac',
+  //       'A400': '#acacb8',
+  //       'A700': '#0b0b0d'
+  //   };
     
-    $mdThemingProvider
-        .definePalette('customBackground', 
-                        customBackground);
+  //   $mdThemingProvider
+  //       .definePalette('customBackground', 
+  //                       customBackground);
 
-   $mdThemingProvider.theme('default')
-       .primaryPalette('customPrimary')
-       .backgroundPalette('customBackground')
+  //  $mdThemingProvider.theme('default')
+  //      .primaryPalette('customPrimary')
+  //      .backgroundPalette('customBackground')
 });
