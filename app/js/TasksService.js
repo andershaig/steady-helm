cb03301a3be7e18b6678767fb0233d72cba7684a
Anders Haig <andershaig@gmail.com>
2016-06-05 18:25:41 -0700

    Add some sample tasks

--- app/js/TasksService.js
+++ app/js/TasksService.js
@@ -15,19 +15,31 @@
   function TasksService($q) {
     var tasks = [
       {
-        name: 'Lia Lugo',
-        avatar: 'svg-1',
-        content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
+        text: 'Morning Routine',
+        subTasks: [
+          {
+            text: 'Brush teeth',
+            isComplete: false
+          },
+          {
+            text: 'Walk dogs',
+            isComplete: false
+          },
+          {
+            text: 'Eat breakfast',
+            isComplete: false
+          }
+        ],
+        isComplete: false,
+        createdTime: new Date(),
+        priority: 1
       },
       {
-        name: 'George Duke',
-        avatar: 'svg-2',
-        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
-      },
-      {
-        name: 'Gener Delosreyes',
-        avatar: 'svg-3',
-        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
+        text: 'Take vitamins'
+        subTasks: [],
+        isComplete: false,
+        createdTime: new Date(),
+        priority: 2
       }
     ];
 
